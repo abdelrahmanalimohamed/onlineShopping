a2bd454f4084d70a0240496254f2600d3abe18d8
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2019-02-28 10:21:14 +0200

    OnlineShopping

---
+++ DeluxeProject/Controllers/AdminController/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeluxeProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+
+        public ActionResult Brands()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+
+        public ActionResult Supplier()
+        {
+            return View();
+        }
+    
+    }
+}
---
+++ DeluxeProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeluxeProject.MvcApplication" Language="C#" %>
