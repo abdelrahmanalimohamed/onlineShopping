ed2bc050bbd95198c1b68c7725a633c378e208ab
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2019-03-03 22:44:20 +0200

    updated

--- DeluxeProject/Controllers/UserController/ThirdPartyController.cs
+++ DeluxeProject/Controllers/UserController/ThirdPartyController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPSnippets.FaceBookAPI;
 
 namespace DeluxeProject.Controllers.UserController
 {
     public class ThirdPartyController : Controller
     {
-        
+        [HttpPost]
+        [AllowAnonymous]
+        public EmptyResult Login()
+        {
+            FaceBookConnect.Authorize("user_photos,email", string.Format("{0}://{1}/{2}", Request.Url.Scheme, Request.Url.Authority, ""));
+
+            return new EmptyResult();
+        }
     }
 }
--- DeluxeProject/Controllers/UserController/UserActionsController.cs
+++ DeluxeProject/Controllers/UserController/UserActionsController.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using ASPSnippets.FaceBookAPI;
+using DeluxeModel;
 
 namespace DeluxeProject.Controllers.UserController
 {
     public class UserActionsController : Controller
     {
+
        public ActionResult SignUp()
         {
+            FaceBookConnect.API_Key = "824415404557890";
+            FaceBookConnect.API_Secret = "8f48f5360b2600fb2744af16d903c28e";
+            user user = new user();
+           
+            if (Request.QueryString["code"] == "access_denied")
+            {
+                ViewBag.Message = "user has denied access";
+            }
+            else
+            {
+                string code = Request.QueryString["code"];
+                if (!string.IsNullOrEmpty(code))
+                {
+                    string data = FaceBookConnect.Fetch(code, "me?fields=name,email");
+                    user = new JavaScriptSerializer().Deserialize<user>(data);
+                   
+                }
+            }
             return View();
         }
 
