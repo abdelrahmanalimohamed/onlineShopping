f2167db577e3bb24275b55ce5981a32ce635bf07
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2019-04-05 05:01:03 +0200

    update

--- DeluxeProject/Controllers/DemoController.cs
+++ DeluxeProject/Controllers/DemoController.cs
@@ -18,16 +18,44 @@ namespace DeluxeProject.Controllers
 
         public ActionResult Details(int id)
         {
+            TempData["itemid"] = id;
             return PartialView("_Details", db.products.Find(id));
         }
 
-        [HttpPost]
-        public ActionResult Details(product product)
+        public ActionResult Update(product product)
         {
-            return null;
+          
+            if (product.ID > 0)
+            {
+                var productdata = (from a in db.products
+                                   where a.ID == product.ID
+                                   select a).FirstOrDefault();
+
+                if(productdata != null)
+                {
+                    productdata.prd_name = product.prd_name;
+                    productdata.prd_desc = product.prd_desc;
+                    productdata.prd_quantity = product.prd_quantity;
+                    productdata.price = product.price;
+                }
+              
+             //   var update = db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+
+                db.SaveChanges();
+
+                return Json("Update Sucess", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("Update Failed", JsonRequestBehavior.AllowGet);
+            }
+        
         }
 
-        [HttpDelete]
+
+
+
+        [HttpPost]
         public ActionResult DeleteProduct(int id)
         {
             var deleteproduct = (from a in db.products
